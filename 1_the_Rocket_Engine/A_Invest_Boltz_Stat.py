bc5b6e94b7d43aee5b8ab66fdb5dadb6ff5ef59c
Haakooto <hakonolav.torvik@gmail.com>
2019-08-25 20:38:41 +0200

    implement maxwell-boltzman dist

--- 1_the_Rocket_Engine/A_Invest_Boltz_Stat.py
+++ 1_the_Rocket_Engine/A_Invest_Boltz_Stat.py
@@ -4,13 +4,27 @@ import matplotlib.pyplot as plt
 def f(mu, sig, x):
 	exp = -0.5 * (x - mu)**2 * sig**(-2)
 	return (np.sqrt(2 * np.pi) * sig)**(-1) * np.exp(exp)
-			
+
 def P(a, b, mu, sig):
 	dx = 0.001
 	n = int((b - a) / dx)
 	x = np.linspace(a, b, n) + mu
 	y = f(mu, sig, x)
 	return x, y, np.trapz(y, dx = dx)
+
+def MaxBoz(v, m = 2.0159 * 1.66e-27, T = 3000):
+	kB = 1.380649e-23
+	e = - 0.5 * m * v ** 2 * (kB * T) ** (-1)
+	p1 = m ** (3/2) * (2 * np.pi * kB * T) ** (-3/2)
+	p3 = 4 * np.pi * v ** 2
+	return p1 * np.exp(e) * p3
+
+def PMaxBoz(a, b):
+	dx = 0.001
+	n = int((b - a) / dx)
+	x = np.linspace(a, b, n)
+	y = MaxBoz(x)
+	return x, y
 
 def FWHM(X, Y, sig):
 	if X.shape != Y.shape:
@@ -29,10 +43,15 @@ def FWHM(X, Y, sig):
 	return a,s,d
 
 
-x, y, A = P(-3,3,0,1)
-print(A)
-a, s, d = FWHM(x, y, 1)
-print(s,d)
+#x, y, A = P(-3,3,0,1)
+#print(A)
+#a, s, d = FWHM(x, y, 1)
+#print(s,d)
 # plt.plot(x,y)
 # plt.grid()
 # plt.show()
+
+x, y = PMaxBoz(-10,10)
+plt.plot(x,y)
+plt.grid()
+plt.show()
