39d2787fe070fd3cc5c51412d175c8caea6aebc3
tobiasob9801 <tobiasob@student.matnat.uio.no>
2019-08-26 21:12:14 +0200

    Finished part 1A

--- 1_the_Rocket_Engine/A_Invest_Boltz_Stat.py
+++ 1_the_Rocket_Engine/A_Invest_Boltz_Stat.py
@@ -1,20 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mass = 3.347E-27
+N = 1E5
+m = 3.347E-27
 T = 3000
 k = 1.38E-23
+sig_b = np.sqrt(k*T/m)
 
-def f(mu, sig, x):
+
+def f1(mu, sig, x):
 	exp = -0.5 * (x - mu)**2 * sig**(-2)
 	return (np.sqrt(2 * np.pi) * sig)**(-1) * np.exp(exp)
 
-def P(a, b, mu, sig):
-	dx = 0.001
-	n = int((b - a) / dx)
+def f2(m,T,x):
+	return (m/(2*np.pi*k*T))**(3/2)*np.exp(-m*x**2/(2*k*T))*4*np.pi*x**2
+
+def P(a, b, mu, sig,f):
+	n = 1E4+1
+	dx = (b-a)/n
 	x = np.linspace(a, b, n) + mu
 	y = f(mu, sig, x)
 	return x, y, np.trapz(y, dx = dx)
+
+sig = 1
+def test_P():
+	s1 = P(-sig,sig,0,sig,f1)[-1]
+	s2 = P(-2*sig,2*sig,0,sig,f1)[-1]
+	s3 = P(-3*sig,3*sig,0,sig,f1)[-1]
+	assert abs(0.68-s1)<0.005
+	assert abs(0.95-s2)<0.005
+	assert abs(0.997-s3)<0.0005
+test_P()
 
 def MaxBoz(v, m = 2.0159 * 1.66e-27, T = 3000):
 	kB = 1.380649e-23
@@ -24,12 +40,13 @@ def MaxBoz(v, m = 2.0159 * 1.66e-27, T = 3000):
 	return p1 * np.exp(e) * p3
 
 def PMaxBoz(a, b):
-	dx = 0.001
-	n = int((b - a) / dx)
+	n = 1E4+1
+	dx = (b-a)/n
 	x = np.linspace(a, b, n)
 	y = MaxBoz(x)
 	return x, y
 
+"""
 def FWHM(X, Y, sig):
 	if X.shape != Y.shape:
 		return False
@@ -45,15 +62,25 @@ def FWHM(X, Y, sig):
 	s = 2*np.sqrt(2*np.log(2))*sig
 
 	return a,s,d
+"""
 
 width = 2.5E4
-x, y, A = P(-width,width,0,np.sqrt(k*T/mass))
-#print(A)
-#a, s, d = FWHM(x, y, 1)
-#print(s,d)
+x, y, A = P(-width,width,0,sig_b,f1)
 plt.plot(x,y)
 plt.grid()
 plt.show()
+
+B = P(5E3,3E4,0,sig_b,f1)[-1]
+print(B) #0.07755
+print(B*N) #7755.38
+
+xv,yv,sv = P(0,3E4,m,T,f2)
+plt.plot(xv,yv)
+plt.grid()
+plt.show()
+#a, s, d = FWHM(x, y, 1)
+#print(s,d)
+
 
 x, y = PMaxBoz(-10,10)
 plt.plot(x,y)
