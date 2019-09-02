c68e2901c0f8c383e4d58b28c87ca07bfcbc48c4
Haakooto <hakonolav.torvik@gmail.com>
2019-09-02 14:11:20 +0200

    small changes

--- 1_the_Rocket_Engine/launch.py
+++ 1_the_Rocket_Engine/launch.py
@@ -1,96 +1,72 @@
 import numpy as np
-from Engine import Engine
+import matplotlib.pyplot as plt
+import sys, os
+
+from engine import Engine
+from rocket import Rocket
+
 import ast2000tools.utils as util
 import ast2000tools.constants as const
 from ast2000tools.space_mission import SpaceMission
 from ast2000tools.solar_system import SolarSystem
-import matplotlib.pyplot as plt
 
 #util.check_for_newer_version()
+
 np.random.seed(14441111)
 seed = util.get_seed("haakooto")
 
 mission = SpaceMission(seed)
 system = SolarSystem(seed)
 
-G = const.G
-mass_p = system.masses[0]*const.m_sun
-r_p = system.radii[0]*1000
 
-def escape_v(r):
-    return np.sqrt(2 * G * mass_p / r)
-
-mass = mission.spacecraft_mass
-area = mission.spacecraft_area
-
-# Variables for rocketengine
-T = 10000 #temperature in K
-L = 1e-5 #lengt of box in m
-N = int(1e6) #number of particles
-
-Ne = area / L ** 2 #numer of engineboxes
-nozzle = 0.5
-
-dt = 1e-12
+# Variables for engine
+N = int(1e5)	# number of particles
+nozzle = 0.25	# nozzle size, as percent of surface
+L = 1e-7 		# lengt of box in m
+T = 3000 		# temperature in K
+dt_e = 1e-12
 ts = 1000
 
-inp = [N, Ne, nozzle, T, L, dt, ts]
+# Variables for rocket
+mass = mission.spacecraft_mass		# rocket drymass
+R = system.radii[0] * 1000			# planet radius
+M = system.masses[0] * const.m_sun	# planet mass
+dt_r = 0.01
 
-rocket = Engine()
-rocket.build(*inp)
-rocket.ignite()
-rocket.performance(mass, escape_v(r_p))
+rocket_area = mission.spacecraft_area
+Ne = rocket_area / L ** 2 / 14 #numer of engineboxes
+fuel_load = 40000
 
-fuel_mass = rocket.dm
-print("loaded fuel: ", fuel_mass)
-m0 = mass + fuel_mass
-r0 = r_p
+# Packages to build rocket and engine
+engine_build = [N, nozzle, T, L, dt_e, ts]
+rocket_build = [mass, R, M, dt_r]
 
-def acc_func(r,m):
-    return rocket.thrust/m - G*mass_p/r**2
 
-N_loop = 100000
-r = np.zeros(N_loop)
-v = np.zeros(N_loop)
-a = np.zeros(N_loop)
-r[0] = r0
+Volcano = Rocket(*rocket_build)
+Mercum = Engine()
 
-i = 0
-dt = 0.01
-m_live = m0
-while v[i]<escape_v(r[i]) and i < N_loop-1:
-    # a[i] = acc_func(r[i],m_live)
-    # v[i+1] = v[i] + a[i]*dt
-    # r[i+1] = r[i] + v[i+1]*dt
-    # m_live -= rocket.consume*dt
-    # i += 1
+Mercum.build(*engine_build)
+Volcano.assemble(Mercum, fuel_load, Ne)
 
-    A0 = acc_func(r[i], m_live)
-    r[i + 1] = r[i] + v[i] * dt + 0.5 * A0 * dt ** 2
-    A1 = acc_func(r[i + 1], m_live)
-    v[i + 1] = v[i] + 0.5 * (A0 + A1) * dt
-    m_live -= rocket.consume*dt
-    i += 1
+Volcano.launch()
 
-v = v[:i+1]
-r = r[:i+1]
-a = a[:i+1]
-a[-1]=a[-2]
 
-pos = r[-1]
-vel = v[-1]
-acc = a[-1]
-space_time = dt*i
-fuel_used = rocket.consume*space_time
-print("time to space: ",space_time)
-print("fueluse: ",fuel_used)
+thrust = Volcano.engine.thrust		# thrust pr box
+dm = Volcano.engine.consume			# mass loss rate
+Ne = Ne 							# number of boxes
+fuel = fuel_load					# loaded fuel
+T1 = Volcano.time 					# launch duration
+planet_pos = system.initial_positions[:,0]
+pos_x = planet_pos[0] + R / const.AU# x-position relative to star3
+pos_y = planet_pos[1]
+T0 = 0								# start of launch
 
-print(rocket.thrust/m0-G*mass_p/r_p**2)
+verify = [thrust, dm, Ne, fuel, T1, (pos_x, pos_y), T0]
 
-# t = np.linspace(0,space_time,i+1)
-# plt.plot(t,v)
-# plt.show()
-# plt.plot(t,r)
-# plt.show()
-# plt.plot(t,a)
-# plt.show()
+mission.set_launch_parameters(*verify)
+mission.launch_rocket()
+
+orb_speed = np.asarray([0,10.194/(86400*365.2422)]) #orbital speed in AU/s
+rot_speed = np.asarray([0,2*np.pi*R/(system.rotational_periods[0]*86400*const.AU)])
+final_position = np.asarray([planet_pos[0] + Volcano.r / const.AU, pos_y]) + orb_speed*T1 + rot_speed*T1
+mission.verify_launch_result(final_position)
