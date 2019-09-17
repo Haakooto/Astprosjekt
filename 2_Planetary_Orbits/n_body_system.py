6bd296917f6357795170a44752ccac440e672ef6
Haakooto <hakonolav.torvik@gmail.com>
2019-09-18 00:54:01 +0200

    made it truly n-body; not all-body, fix minor bug, tried to make plot with right names; gave up

--- 2_Planetary_Orbits/n_body_system.py
+++ 2_Planetary_Orbits/n_body_system.py
@@ -16,17 +16,39 @@ from ast2000tools.solar_system import SolarSystem
 
 
 class SolarSys(SolarSystem):
-	def __init__(self, seed, data_path=None, has_moons=True, verbose=True):
+	def __init__(self, seed, N, data_path=None, has_moons=True, verbose=True):
 		SolarSystem.__init__(self, seed, data_path=None, has_moons=True, verbose=True)
 		self.one_year = np.sqrt(self.semi_major_axes[0] ** 3 / self.star_mass)
 		self.one_years = np.sqrt(self.semi_major_axes ** 3 / self.star_mass)
 
-	def two_body_system(self, yrs, dt_pr_yr):
+		dists = np.linalg.norm(self.initial_positions, axis = 0)
+		self.non_ordered_planets = np.argsort(dists)[::-1]
+
+		if type(N) == "str":
+			N = self.number_of_planets
+
+		las = self.find_largest_attractor()
+		self.planets = las[:N]
+		self.my_number_of_planets = N
+		self.my_masses = self.masses[self.planets]
+		self.my_initial_positions = self.initial_positions[:, self.planets]
+		self.my_initial_velocities = self.initial_velocities[:, self.planets]
+
+	def find_largest_attractor(self):
+		F = (
+			-const.G_sol
+			* self.star_mass
+			* self.masses
+			* np.linalg.norm(self.initial_positions, axis=0) ** -2
+		)
+		return np.argsort(F)
+
+	def N_body_system(self, yrs, dt_pr_yr):
 		def A(r):
 			R_p = r[0] - r[1:]
-			F_p = -const.G_sol * R_p * self.star_mass * np.transpose([self.masses]*2) * np.transpose([np.linalg.norm(R_p, axis = 1) ** -3]*2)
+			F_p = -const.G_sol * R_p * self.star_mass * np.transpose([self.my_masses]*2) * np.transpose([np.linalg.norm(R_p, axis = 1) ** -3]*2)
 			A_s = np.sum(F_p, axis=0) / self.star_mass
-			A_p = -F_p / np.transpose([self.masses]*2)
+			A_p = -F_p / np.transpose([self.my_masses]*2)
 			return np.concatenate(([A_s], A_p))
 
 		T = self.one_year * yrs
@@ -35,20 +57,18 @@ class SolarSys(SolarSystem):
 
 		self.time = np.linspace(0, T, n)
 
-		# self.all_masses = np.concatenate((self.star_mass, self.masses))
+		Rcm = np.sum(self.my_masses * self.my_initial_positions, axis=1) / (sum(self.my_masses) + self.star_mass)
+		Vcm = np.sum(self.my_masses * self.my_initial_velocities, axis=1) / (sum(self.my_masses) + self.star_mass)
 
-		Rcm = np.sum(self.masses * self.initial_positions, axis=1) / (sum(self.masses) + self.star_mass)
-		Vcm = np.sum(self.masses * self.initial_velocities, axis=1) / (sum(self.masses) + self.star_mass)
-
-		R = np.zeros((n, self.number_of_planets + 1, 2))  # [time][object][coord]
+		R = np.zeros((n, self.my_number_of_planets + 1, 2))  # [time][object][coord]
 		R[0, 0] = -Rcm  # moving sun
-		R[0, 1:, 0] = self.initial_positions[0] - Rcm[0]  # setting planet pos
-		R[0, 1:, 1] = self.initial_positions[1] - Rcm[1]  # setting planet pos
+		R[0, 1:, 0] = self.my_initial_positions[0] - Rcm[0]  # setting planet pos
+		R[0, 1:, 1] = self.my_initial_positions[1] - Rcm[1]  # setting planet pos
 
 		V = np.zeros_like(R)  # [time][object][coord]
 		V[0, 0] = -Vcm  #
-		V[0, 1:, 0] = self.initial_velocities[0] - Vcm[0]  # setting planet vel
-		V[0, 1:, 1] = self.initial_velocities[1] - Vcm[1]  # setting planet vel
+		V[0, 1:, 0] = self.my_initial_velocities[0] - Vcm[0]  # setting planet vel
+		V[0, 1:, 1] = self.my_initial_velocities[1] - Vcm[1]  # setting planet vel
 
 		for t in range(n - 1):
 
@@ -76,7 +96,7 @@ class SolarSys(SolarSystem):
 	# 	plt.show()
 
 	def radial_vel(self, i=np.pi):
-		xvel = np.sin(i) * self.vel[:, 0, 0]
+		xvel = np.sin(i) * self.vels[:, 0, 0]
 		noise = np.random.normal(0, 0.2 * xvel.max(), len(xvel))
 		V = 1444
 		self.vnoise = xvel + noise + V
@@ -91,8 +111,8 @@ class SolarSys(SolarSystem):
 		planet_names = ["Vulcan", "Laconia", "Vogsphere", "Ilus", "Alderaan", "Apetos", "Auberon", "Zarkon", "Tellusia", "X"]
 
 		plt.plot(*S, color=np.array(self.star_color) / 255, label="Sun")
-		for p in range(self.number_of_planets):
-			lab = planet_names[p]
+		for p in range(self.my_number_of_planets):
+			lab = planet_names[self.non_ordered_planets[p]]
 			plt.plot(*P[p], "c", label=lab)
 		plt.scatter([0], [0], label="Centre of mass")
 
@@ -102,26 +122,24 @@ class SolarSys(SolarSystem):
 		plt.show()
 
 	def assemble_data(self):
-		first = np.concatenate(([self.star_mass], self.vnoise))
-		sec = np.concatenate(([self.p_mass], self.time))
-		data = np.concatenate(([first], [sec]))
-		np.save("radial_velocity_curve.npy", data)
+		data = np.concatenate(([self.vnoise], [self.time]))
+		np.save("radial_velocity_curve_multiple.npy", data)
 
 
 
 if __name__ == "__main__":
-	seed = util.get_seed("mikkelme")
+	seed = util.get_seed("haakooto")
 
-	system = SolarSys(seed)
+	system = SolarSys(seed, 4)
 
-	yrs = 2220
-	dt = 1e-4
+	yrs = 40
+	dt = 1e-3
 
-	system.two_body_system(yrs, dt)
-	system.plot_n_pos()
+	system.N_body_system(yrs, dt)
+	# system.plot_n_pos()
 	# system.energy_conserve()
-	# system.radial_vel(i=2*np.pi/3)
-	# system.assemble_data()
+	system.radial_vel(i=2*np.pi/3)
+	system.assemble_data()
 
 	# np.save("star_data.npy", system.vnoise)
 	# np.save("times.npy", system.time)
