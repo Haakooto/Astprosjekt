1359a5a7c23cdfb7b651c4956c9993cbab838bc5
haakooto <hakonolav.torvik@gmail.com>
2019-12-03 18:26:22 +0100

    imporve on lading

--- 7_Landing/landing.py
+++ 7_Landing/landing.py
@@ -20,8 +20,7 @@ sys.path.append(os.path.abspath("../6_Preparing_for_Landing"))
 import launch
 from orbits import SolarSys
 from navigate import navigate
-# from rocket import Rocket as Rocket
-from journey import Rocket as Rocket
+from journey import Rocket
 from atmosphere import density
 
 import ast2000tools.utils as util
@@ -32,9 +31,9 @@ from ast2000tools.solar_system import SolarSystem
 
 class Landing:
 	def __init__(self, r0, v0, t0, system, mission):
-		self.r = np.transpose(r0)
-		self.v = np.transpose(v0)
-		self.t = np.asarray(t0)
+		self.r = np.transpose(r0.reshape((1,3)))
+		self.v = np.transpose(v0.reshape((1,3)))
+		self.t = np.asarray(t0).reshape((1,))
 
 		self.sys = system
 		self.mis = mission
@@ -47,18 +46,25 @@ class Landing:
 		self.parachute_deployed = False
 
 	def F_d(self, r, v):
+		C_d = 1
 		if self.parachute_deployed:
 			A = 2 * const.G * self.M * self.m / (self.rho_at_0 * (3 * self.R) ** 2)
 		else:
-			A = self.sys.lander_area
+			A = self.sys.mission.lander_area
 		omega = 2 * np.pi / (self.sys.rotational_periods[self.mis.dest] * const.day)
 		w = omega * np.asarray([-r[1], r[0], 0])
 		v_d = v - w
-		dens = density(np.linalg.norm(r) - R)
+		dens = density(np.linalg.norm(r) - self.R)
 		return dens / 2 * C_d * A * np.linalg.norm(v_d) * (-v_d)
 
+	def surface(self):
+		h = np.linspace(0, 2 * np.pi, 1000)
+		x = self.R * np.cos(h)
+		y = self.R * np.sin(h)
+		plt.plot(x, y)
+
 	def gravity(self, r):
-		return r * const.G * self.M * np.linalg.norm(r) ** -3
+		return -r * const.G * self.M * np.linalg.norm(r) ** -3
 
 	def rddot(self, r, v):
 		if np.linalg.norm(r) <= self.R:
@@ -77,7 +83,7 @@ class Landing:
 	def free_fall(self, T, dt):
 		nT = int(T / dt)
 
-		t = np.linspace(self.t[-1], T, nT)
+		t = np.linspace(self.t[-1], T, nT, endpoint=False)
 		t0 = t[0]
 		r0 = self.r[:, -1]
 		v0 = self.v[:, -1]
@@ -90,8 +96,13 @@ class Landing:
 		r, v = np.split(falled, 2)
 
 		self.r = np.concatenate((self.r, r), axis=1)
-		self.v = v[-1]
+		self.v = v[:, -1]
 		self.t = np.concatenate((self.t, t))
+
+	def plot(self):
+		x, y, z = self.r
+		plt.plot(x, y)
+		plt.show()
 
 
 if __name__ == "__main__":
@@ -118,8 +129,10 @@ if __name__ == "__main__":
 	time = 0.11598196795767118
 	r_pos = np.asarray([0.12979, 0.157862])
 	r_vel = np.asarray([-6.74248, 6.84742])
+	Volcano.begin_interplanetary_journey(system, mission, destination=destination, verbose=False)
 	Volcano.teleport(time, r_pos, r_vel)
-	Volcano.begin_interplanetary_journey(system, mission, destination=destination)
+	Volcano.boost(Volcano.enter_stable_orbit_boost())
+
 
 	"""
 	To have accurate planet position and velocity at time we use
@@ -132,9 +145,16 @@ if __name__ == "__main__":
 	p_vel = system._compute_single_planet_velocity(time, destination)
 
 	r0 = r_pos - p_pos
-	v0 = r_vel - p_vel
+	r0 = np.array([*r0, 0]) * const.AU
+	v0 = Volcano.vel - p_vel
+	v0 = np.array([-0.3, 0.3])
+	v0 = np.array([*v0, 0]) * const.AU / const.yr
 
 	landing = Landing(r0, v0, Volcano.travel_time, system, Volcano)
+	landing.free_fall(10000, 1e-4)
+	landing.surface()
+	landing.plot()
+
 
 
 
