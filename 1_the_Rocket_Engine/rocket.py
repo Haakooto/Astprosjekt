5a952b54d5761634f6a3ba91b2f8007051a931dc
haakooto <hakonolav.torvik@gmail.com>
2019-12-16 14:26:32 +0100

    fix typo

--- 1_the_Rocket_Engine/rocket.py
+++ 1_the_Rocket_Engine/rocket.py
@@ -78,7 +78,7 @@ class Rocket:
     def fuel_use(self, dv):
         # Equation derived in part 5, "Interplanetary spacetravel"
         dv = np.linalg.norm(dv)
-        dm = (self.mass + self.fuel) * (1 - np.exp[-dv / self.engine.exhaust_v])
+        dm = (self.mass + self.fuel) * (1 - np.exp(-dv / self.engine.exhaust_v))
 
         return dm
 
